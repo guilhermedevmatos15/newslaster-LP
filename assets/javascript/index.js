30eb4753dd3b965d022989717678203ff8915360
Guilherme Magno Matos <115946540+guilhermedevmatos15@users.noreply.github.com>
2023-01-08 20:32:06 -0300

    up

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,14 +1,5 @@
 const menu = document.querySelector('i.fa-bars');
 const div = document.querySelector('div.menu');
-const body = document.querySelector('body');
-
-body.addEventListener('resize', ()=>{
-   if (window.innerWidth >= 860) {
-      div.classList.remove('active');
-   } else {
-      div.classList.add('active');
-   }
-});
 
 menu.addEventListener('click', () => {
    if (div.classList[1] === undefined) {
